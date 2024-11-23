a2b21962b9ff330ccf72b8812ba116a736cdf830
Anton <roundronin8@gmail.com>
2024-11-23 11:04:56 +0300

    feat(DAL)/EF-implementation
    Implements repositories for previously defined interfaces in the context of using a database. Not tested.

--- DAL/Entities/Product.cs
+++ DAL/Entities/Product.cs
@@ -5,7 +5,7 @@ namespace DAL.Entities;
 public class Product : IEntity<int>
 {
     public int Id { get; set; }
-    public string Name { get; set; } = null!;
+    public required string Name { get; set; }
 
     public ICollection<Inventory> Stores { get; set; } = [];
 }
